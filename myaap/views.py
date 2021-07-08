df4b44f83890096d78c2be695a7771a7128d5688
Annii <anish.1903.kmr@gmail.com>
2021-07-08 17:13:52 +0530

    builded more

--- myaap/views.py
+++ myaap/views.py
@@ -1,5 +1,19 @@
+import requests
+from requests import quote_plus
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
 # Create your views here.
+
+BASE_WEB_URL = 'https://chandigarh.craigslist.org/search/?query={}'
+
 def home(request):
     return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    response = requests.get('https://chandigarh.craigslist.org/search/bbb?query=python&areaAbb=chandigarh')
+    print(quote_plus(search))
+    search_result = {
+        'search': search,
+    }
+    return render(request, 'myaap/new_search.html', search_result)
