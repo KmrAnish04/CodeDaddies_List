da91d19d5c7fadf11cf7ed946e851b36ce9517a4
Annii <anish.1903.kmr@gmail.com>
2021-07-09 14:22:03 +0530

    added some in viws.py

--- myaap/views.py
+++ myaap/views.py
@@ -1,18 +1,33 @@
-import requests
-from requests import quote_plus
 from django.shortcuts import render
+from requests.api import post
 from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+from . import models
 # Create your views here.
 
-BASE_WEB_URL = 'https://chandigarh.craigslist.org/search/?query={}'
+BASE_WEB_URL = 'https://chandigarh.craigslist.org/search/lss?query={}'
 
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
-    response = requests.get('https://chandigarh.craigslist.org/search/bbb?query=python&areaAbb=chandigarh')
-    print(quote_plus(search))
+    models.Search.objects.create(Search=search)
+    final_url = BASE_WEB_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    post_title = post_listings[0].find(class_='result-title').text
+    post_url = post_listings[0].find('a').get('href')
+    post_price = post_listings[0].find(class_='result-price').text
+    
+    print(post_title)
+    print(post_url)
+    print(post_price)
+
     search_result = {
         'search': search,
     }
